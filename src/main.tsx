f9c017943a65a737af6edad8c863f0ec36d04775
timmyproofficial <timmyshow96@gmail.com>
2024-07-23 19:19:07 +0100

    Setup React Query and React Devtools.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import router from './routes';
+import './index.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     {/* <ChakraProvider> */}
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
     {/* </ChakraProvider> */}
   </React.StrictMode>
 );
