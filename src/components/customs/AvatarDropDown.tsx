9fba70618db4f442052ee0bef490d0b104d9b288
timmyproofficial <timmyshow96@gmail.com>
2024-08-31 01:41:41 +0100

    Add dropdown for user avatar

---
+++ src/components/customs/AvatarDropDown.tsx
@@ -0,0 +1,49 @@
+import { LogOut, LogIn, User } from 'lucide-react';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Avatar, AvatarFallback } from '../ui/avatar';
+import { AuthUser } from '@/entities/auth';
+
+interface Props {
+  user: AuthUser;
+  mutate: () => void;
+}
+
+export function AvatarDropDown({ user, mutate }: Props) {
+  if (!user) return <LogIn />;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        {/* <Button variant="outline">Open</Button> */}
+        <Avatar className="cursor-pointer">
+          <AvatarFallback>BA</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>
+              {user?.firstName} {user?.lastName}
+            </span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => mutate()}>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- src/components/customs/Navbar.tsx
+++ src/components/customs/Navbar.tsx
@@ -4,18 +4,22 @@ import { Link } from 'react-router-dom';
 import ColorModeSwitch from './ColorModeSwitch';
 import { Button } from '../ui/button';
 import useLogout from '@/hooks/useLogout';
+import { AvatarDropDown } from './AvatarDropDown';
+import userAuthStore from '@/store';
 
 const Navbar = () => {
+  const { user } = userAuthStore();
   const { mutate, isLoading, error } = useLogout();
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between items-center">
       <Link to="/">
         <Image src={logo} alt="D818 Logo" boxSize="60px" />
       </Link>
       {/* <ColorModeSwitch /> */}
-      <Button className="mt-3" onClick={() => mutate()}>
+      {/* <Button className="mt-3" onClick={() => mutate()}>
         Logout
-      </Button>
+      </Button> */}
+      <AvatarDropDown user={user} mutate={mutate} />
     </div>
   );
 };
