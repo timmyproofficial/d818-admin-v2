c036d7ab831d59f3ed63942244a48f049298010c
timmyproofficial <timmyshow96@gmail.com>
2024-09-02 15:13:19 +0100

    Remove used imports

--- src/hooks/useAdmins.ts
+++ src/hooks/useAdmins.ts
@@ -1,7 +1,5 @@
-import getAdminData from '@/data/admins';
 import { useQuery } from '@tanstack/react-query';
 import { Admin } from '@/entities/admin';
-import { useEffect, useState } from 'react';
 import APIClient from '@/services/apiClient';
 
 const apiClient = new APIClient<Admin>('/admins');
